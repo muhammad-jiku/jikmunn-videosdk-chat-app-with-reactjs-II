d9696aa309018c612c61ea6a9815ad42a334b2b7
muhammad-jiku <muhammadjiku364@gmail.com>
2022-09-01 21:13:53 +0600

    changes occured due to token expiration

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import generateMuiTheme from './mui/theme';
-import { ThemeProvider } from '@material-ui/styles';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import generateMuiTheme from "./mui/theme";
+import { ThemeProvider } from "@material-ui/styles";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter>
+ReactDOM.render(
+  <React.StrictMode>
     <ThemeProvider theme={generateMuiTheme()}>
       <App />
     </ThemeProvider>
-  </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
